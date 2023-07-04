c685106b343f12e95840fbe483b0827ba63edb6e
riidefi <34194588+riidefi@users.noreply.github.com>
2023-07-04 16:43:40 -0600

    Fix MacOS build

--- src/write_to_file.cpp
+++ src/write_to_file.cpp
@@ -5,7 +5,7 @@
 #include <stddef.h>
 
 extern "C" {
-  void WriteToOpenFileC(const char* path, size_t path_len, const char* data, size_t data_len);
+  void WriteToOpenFileC(const char* path, uint64_t path_len, const char* data, uint64_t data_len);
 }
 
 // Avoiding string_view, span...
